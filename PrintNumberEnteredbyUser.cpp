bbfd72d92bb0e11abeacf3ded23134ca1f624805
jahirnstu14 <jahirkhan.ice.nstu.student14@gmail.com>
2022-08-22 10:40:27 +0600

    add to the cool

---
+++ PrintNumberEnteredbyUser.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer:wgt yuiuggff ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
